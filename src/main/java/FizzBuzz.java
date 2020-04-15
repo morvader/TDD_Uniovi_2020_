187bd3b5ff10a8d147b77e4fe1284e491384fcb9
Francisco Moreno <francisco.moreno@sngular.com>
2020-04-15 18:00:30 +0200

    tests

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,8 @@
 public class FizzBuzz {
 
     public String getFizzBuzzCode(int numero) {
+        if(numero % 3 == 0)
+            return "Fizz";
         return "1";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,16 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp(){
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void unNumeroNormal_DevuelveElNumero(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         int numero = 1;
-        String retorno = fizzbuzz.getFizzBuzzCode(numero);
+        String retorno = fizzBuzz.getFizzBuzzCode(numero);
 
         assertEquals("El código retornado no es correcto","1", retorno);
     }
 
+    @Test
+    public void unMultiploDeTres_DevuelveFizz(){
+        int numero = 3;
+        String retorno = fizzBuzz.getFizzBuzzCode(numero);
 
+        assertEquals("El código retornado no es correcto","Fizz", retorno);
+    }
 }
